0ae326526f7141c252c91accf6f934c115c1f644
JoshDevx <josmdev@gmail.com>
2025-10-16 21:34:48 -0500

    Segundo commit. Fin de día 1

--- src/arimetica/Arimetica.java
+++ src/arimetica/Arimetica.java
@@ -1,20 +1,13 @@
+package arimetica;
+
 public class Arimetica {
     // Atributos
-    int operando1, operando2;
-
-    // Constructor
-    // Una vez agregando un constructor, este ya no se crea de manera automatica
-    //Asi volvamos a crear un nuevo objeto
-//    public Arimetica(int op1, int op2){
-//        operando1 = op1;
-//        operando2 = op2;
-//    }
+    private int operando1, operando2;
 
     // Usando this
     public Arimetica(int operando1, int operando2){
         this.operando1 = operando1;
         this.operando2 = operando2;
-        System.out.println("Dir. Me This: " + this);
     }
 
     // Podemos crear un nuevo constructor vacio
@@ -24,27 +17,27 @@ public class Arimetica {
     }
 
     // Metodos
-    void sumar(){
+    public void sumar(){
         System.out.println("Suma: " + (this.operando1 + this.operando2));
     }
-    void restar(){
+    public void restar(){
         System.out.println("Restar: " + (this.operando1 - this.operando2));
     }
 
-    public static void main(String[] args) {
-        //Creamos nuestros objetos
-        System.out.println("*** Objeto 1 ***");
-        Arimetica arimetica1 = new Arimetica(5,7);
-
-        arimetica1.sumar();
-        System.out.println("Dir. Men Obj1: " + arimetica1);
-
-        //Creamos nuestros objetos
-        System.out.println("\n*** Objeto 2 ***");
-        Arimetica arimetica2 = new Arimetica(10,30);
-
-        arimetica1.sumar();
-        System.out.println("Dir. Men Obj1: " + arimetica2);
+    // Metodos de acceso
+    public int getOperando1() {
+        return operando1;
     }
 
+    public void setOperando1(int operando1) {
+        this.operando1 = operando1;
+    }
+
+    public int getOperando2() {
+        return operando2;
+    }
+
+    public void setOperando2(int operando2) {
+        this.operando2 = operando2;
+    }
 }
--- src/prueba_arimetica/Prueba_Arimetica.java
+++ src/prueba_arimetica/Prueba_Arimetica.java
@@ -1,4 +1,28 @@
 package prueba_arimetica;
+import arimetica.Arimetica;
 
 public class Prueba_Arimetica {
+
+    public static void main(String[] args) {
+        //Creamos nuestros objetos
+
+        System.out.println("*** Objeto 1 ***");
+        Arimetica arimetica1 = new Arimetica(5,7);
+
+        // Usamos los metodos de acceso
+        System.out.print("Leemos el operador 1 (Usando get): " + arimetica1.getOperando1());
+        System.out.println("\nModificamos con set");
+        //Modificamos con set
+        arimetica1.setOperando1(10);
+        arimetica1.setOperando2(20);
+
+        arimetica1.sumar();
+
+        //
+        System.out.println("\n*** Objeto 2 ***");
+        Arimetica arimetica2 = new Arimetica(10,30);
+
+        arimetica2.sumar();
+    }
+
 }
